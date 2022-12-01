251af1bad20475ecd0b940dd18e0c435f79d4a86
unnamedka1 <agarwalkansihk12345@gmail.com>
2022-12-01 18:12:34 +0530

    upadtes

---
+++ src/components/ExerciseVideos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExerciseVideos = () => {
+    return <div>ExerciseVideo</div>;
+};
+
+export default ExerciseVideos;
---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/Logo.png";
+
+const Footer = () => (
+    <Box mt="80px" bgcolor="#FFF3F4">
+        <Stack
+            gap="40px"
+            sx={{ alignItems: "center" }}
+            flexWrap="wrap"
+            px="40px"
+            pt="24px"
+        >
+            <img
+                src={Logo}
+                alt="logo"
+                style={{ width: "50px", height: "50px" }}
+            />
+        </Stack>
+        <Typography
+            variant="h5"
+            sx={{ fontSize: { lg: "28px", xs: "20px" } }}
+            mt="41px"
+            textAlign="center"
+            pb="40px"
+        >
+            Made with ❤️ by Kanishk Agarwal
+        </Typography>
+    </Box>
+);
+
+export default Footer;
